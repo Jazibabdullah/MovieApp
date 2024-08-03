54b2a2a423d37da232ceb5cec498a7a6fd21f4f7
Jazib Abdullah <mjazibabdullah@gmail.com>
2024-08-03 05:55:14 +0500

    package Name changed
    package name changed
    tested on ios and android
    first android debug build created

--- android/app/src/main/java/com/testing/MainApplication.kt
+++ android/app/src/main/java/com/testing/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.testing
+package com.MovieApp
 
 import android.app.Application
 import com.facebook.react.PackageList
---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null, // If you manually link, or it's auto-linked by RN
+      },
+    },
+  },
+};
